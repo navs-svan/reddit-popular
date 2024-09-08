3b4ab085a7d0ac062cf0ef8ddc82d0c0bf597aaa
navssvan1 <ryannavarro120@gmail.com>
2024-09-08 13:48:51 +0800

    added terraform

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.34.0"
+    }
+  }
+}
+
+
+provider "google" {
+  credentials = file(var.creds)
+  project     = var.project_id
+  region      = var.region
+}
+
+resource "google_storage_bucket" "reddit-bucket" {
+  name          = var.google_storage_bucket_name
+  location      = var.location
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+resource "google_bigquery_dataset" "redit-bq" {
+  dataset_id                 = var.bq_dataset_name
+  location                   = var.location
+  delete_contents_on_destroy = true
+}
