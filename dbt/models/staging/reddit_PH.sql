d838d92269020c198c0a01124dfb6f76c358d3ad
Mark Ryan Navarro <mrlnavarro1@gmail.com>
2024-09-11 14:40:16 +0000

    dbt one main table

---
+++ dbt/models/staging/reddit_PH.sql
@@ -0,0 +1,24 @@
+{{ config(materialized='view') }}
+
+
+select
+    {{ dbt_utils.surrogate_key(['title', 'subreddit', 'author']) }} as post_id,
+    title,
+    subreddit,
+    author,
+    CONCAT('https://www.reddit.com', url) as post_url,
+    nsfw,
+    score,
+    self_text,
+    upvote_ratio,
+    awards,
+    timestamp_seconds(cast(time as integer)) as date_posted
+
+from {{ source('staging', 'PH')}}
+
+{% if var('is_test_run', default=true) %}
+
+    limit 100
+
+{% endif %}
+
